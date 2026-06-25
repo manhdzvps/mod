7db80844c7866bf38b011830283db8efeef8678b
manhdzvps <145472641+manhdzvps@users.noreply.github.com>
2026-06-25 19:02:52 +0000

    Create KingMCAddonCategory.java

---
+++ src/main/java/com/manhdzvps/addon/modules/KingMCAddonCategory.java
@@ -0,0 +1,8 @@
+package com.manhdzvps.addon.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Category;
+import net.minecraft.item.Items;
+
+public class KingMCAddonCategory {
+    public static final Category CATEGORY = new Category("KingMC", Items.DIAMOND_SWORD.getDefaultStack());
+}
