1a8754820545dfd2dfdcb48605ba8fba20743db6
manhdzvps <145472641+manhdzvps@users.noreply.github.com>
2026-06-26 11:32:01 +0700

    Update KingMCAddon.java

--- src/main/java/com/manhdzvps/addon/KingMCAddon.java
+++ src/main/java/com/manhdzvps/addon/KingMCAddon.java
@@ -3,14 +3,22 @@ package com.manhdzvps.addon;
 import com.manhdzvps.addon.modules.*;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.fabricmc.api.ClientModInitializer;
 
-public class KingMCAddon extends MeteorAddon {
+public class KingMCAddon extends MeteorAddon implements ClientModInitializer {
+    public static final KingMCAddon INSTANCE = new KingMCAddon();
+
     @Override
     public void onInitialize() {
         Modules.get().add(new AutoLumberjack());
         Modules.get().add(new AutoJunkDiscard());
         Modules.get().add(new AutoBuyExp());
         Modules.get().add(new AutoRepair());
+    }
+
+    @Override
+    public void onInitializeClient() {
+        onInitialize();
     }
 
     @Override
