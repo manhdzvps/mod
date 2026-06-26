7b1a0c4f3c6b8b74bf16f51badf19299bdb3c25b
manhdzvps <145472641+manhdzvps@users.noreply.github.com>
2026-06-26 11:53:46 +0700

    Update KingMCAddonCategory.java

--- src/main/java/com/manhdzvps/addon/modules/KingMCAddonCategory.java
+++ src/main/java/com/manhdzvps/addon/modules/KingMCAddonCategory.java
@@ -4,5 +4,8 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import net.minecraft.item.Items;
 
 public class KingMCAddonCategory {
-    public static final Category CATEGORY = new Category("KingMC", Items.DIAMOND_SWORD.getDefaultStack());
+    public static final Category CATEGORY =
+        new Category("KingMC", Items.EMERALD.getDefaultStack());
+
+    public static void init() {}
 }
